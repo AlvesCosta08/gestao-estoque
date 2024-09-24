24b435a30e38cb397a35923a2af6ce242d0e6b5c
Alves08 <alvescosta74@gmail.com>
2024-09-24 16:53:36 -0300

    ajuste em ci/cd

--- src/test/java/com/gestao_estoque/gestao_estoque/GestaoEstoqueApplicationTests.java
+++ src/test/java/com/gestao_estoque/gestao_estoque/GestaoEstoqueApplicationTests.java
@@ -2,8 +2,10 @@ package com.gestao_estoque.gestao_estoque;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestaoEstoqueApplicationTests {
 
 	@Test
